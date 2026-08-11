ce04b1f651cc96b0e4e96db23c5bdb777972b699
matthiasbienvenu <matthias.bienvenu@telecom-sudparis.eu>
2026-08-11 11:31:08 +0200

    feat: added edges and nodes columns

--- bin/launch-bench.sh
+++ bin/launch-bench.sh
@@ -1,9 +1,9 @@
-# programName [outputFmt] [numTrials] [length] [width] [pattern] [stride] [maxLength]
+# programName [outputFmt] [numTrials] [length] [width] [topology] [stride] [maxLength]
 # 	outputFmt - program output, default=3 (see below)
 # 	numTrials (per length)
 # 	starting length of the topology
 # 	width - width of the graph topology
-# 	pattern - Structure of graph, default=0 (see below)
+# 	topology - Structure of graph, default=0 (see below)
 # 	stride - how to grow the length between each set of trials
 # 	maxLength - maximum lenght to try
 
@@ -15,23 +15,44 @@
 # 	4: csv data is printed and trials are averaged for each length
 # 	5: csv data is printed and trials are averaged for each length and headers are printed
 
-# Pattern can be:
-# 	0: No interconnect between branches
-# 	1: Adds an extra root node before the initial fork
+# Topology can be:
+# 	0: ParallelChains. <width> independent parallel lines of size <length>
+# 	1: ParallelChainsSingleEntry. Same as 0 but with a additional root node
+# 	2: MeshGraph. <length> layers of size <width> where each node is connected to 2 nodes in the next layer
+# 	3: MeshGraphSingleEntry. Same as 2 but with a additional root node
+# 	4: BinaryTree of depth <length> (the number of nodes is n*(n+1)/2). Width will be ignored.
 
 OUTPUT_FMT=3
 NUM_TRIALS=10
+STRIDE=1
+
+echo "Testing single line graph"
+
 LENGTH=1
 WIDTH=1
 PATTERN=0
-STRIDE=1
-MAX_LENGTH=2000
-
-echo "Testing a single line graph"
-
+MAX_LENGTH=100
 bin/graphPerfScaling "$OUTPUT_FMT" "$NUM_TRIALS" "$LENGTH" "$WIDTH" "$PATTERN" "$STRIDE" "$MAX_LENGTH" >straight-line.csv
 
-echo "Testing a 4 parallel lines graph"
+echo "Testing parallel lines graph"
 
+LENGTH=1
 WIDTH=4
+PATTERN=0
+MAX_LENGTH=100
 bin/graphPerfScaling "$OUTPUT_FMT" "$NUM_TRIALS" "$LENGTH" "$WIDTH" "$PATTERN" "$STRIDE" "$MAX_LENGTH" >${WIDTH}-parallel-lines.csv
+
+echo "Testing mesh graph"
+
+LENGTH=1
+WIDTH=10
+PATTERN=2
+MAX_LENGTH=40
+bin/graphPerfScaling "$OUTPUT_FMT" "$NUM_TRIALS" "$LENGTH" "$WIDTH" "$PATTERN" "$STRIDE" "$MAX_LENGTH" >${WIDTH}-mesh-graph.csv
+
+echo "Testing bintree graph"
+
+LENGTH=1
+PATTERN=4
+MAX_LENGTH=10
+bin/graphPerfScaling "$OUTPUT_FMT" "$NUM_TRIALS" "$LENGTH" "$WIDTH" "$PATTERN" "$STRIDE" "$MAX_LENGTH" >bin-tree.csv
