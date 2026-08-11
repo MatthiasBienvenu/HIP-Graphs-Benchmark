87cb73b28cd19adb7bbb859dd42b6f5337e5fd8e
matthiasbienvenu <matthias.bienvenu@telecom-sudparis.eu>
2026-08-11 16:22:44 +0200

    feat: x_axis for graph.py (also nicer to look at)

--- bin/graph.py
+++ bin/graph.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import cast
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -31,12 +32,19 @@ def parse_args():
         "--columns",
         nargs="+",
         help="Columns to plot (space separated). If omitted, all default columns are plotted.",
+        default=DEFAULT_COLUMNS,
+    )
+    parser.add_argument(
+        "-x",
+        "--x-axis",
+        help="Column to use for the x axis.",
+        default="length",
     )
 
     return parser.parse_args()
 
 
-def load_means_stds(filename, columns):
+def load_means_stds(filename: str, columns: list[str], x_axis: str):
     df = pd.read_csv(filename, skipinitialspace=True).dropna(axis=1)
     df = df.drop(columns=["width", "pattern"])
 
@@ -45,38 +53,49 @@ def load_means_stds(filename, columns):
         print(f"{filename}: unknown column(s): {', '.join(unknown)}")
         return None, None
 
-    df = df[["length"] + columns]
-    vals = df.groupby(["length"]).agg(["mean", "std"])
+    df = df[columns + [x_axis]]
+    vals = df.groupby([x_axis]).agg(["mean", "std"])
     means = vals.xs("mean", axis=1, level=1)
     stds = vals.xs("std", axis=1, level=1)
+
     return means, stds
 
 
 def main():
     args = parse_args()
-    columns = args.columns if args.columns is not None else DEFAULT_COLUMNS
+    columns = cast(list[str], args.columns)
+    x_axis = cast(str, args.x_axis)
+    if x_axis in columns:
+        print(f'Column "{x_axis}" cannot be plotted because it used as the x_axis.')
+        return
 
     data = {}
     for filename in args.filenames:
-        means, stds = load_means_stds(filename, columns)
+        means, stds = load_means_stds(filename, columns, x_axis)
         data[filename] = (means, stds)
 
     axes = {key: plt.subplots()[1] for key in (data if args.transpose else columns)}
     for col in columns:
         for filename, (means, stds) in data.items():
             ax = axes[filename if args.transpose else col]
-            ax.errorbar(
+            ax.plot(
                 means.index,
                 means[col],
-                yerr=stds[col],
-                marker="x",
-                capsize=3,
+                "+-",
                 label=col if args.transpose else filename,
+            )
+            ax.fill_between(
+                means.index,
+                means[col] - stds[col],
+                means[col] + stds[col],
+                alpha=0.2,
+                edgecolor="k",
+                linestyle="--",
             )
 
     for key, ax in axes.items():
         ax.set_title(key)
-        ax.set_xlabel("length")
+        ax.set_xlabel(x_axis)
         ax.set_ylabel("microseconds")
         ax.legend()
 
